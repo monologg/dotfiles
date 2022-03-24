990113222379497d529c6b1e8be0415bc3c3f435
monologg <adieujw@gmail.com>
2022-03-24 13:26:05 +0900

    fix: awscli work well

--- dot_ubuntu.sh
+++ dot_ubuntu.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 # sudo apt-get -y upgrade
 sudo apt-get install -y vim curl awscli python3-pip tmux fonts-powerline git-lfs tree
 sudo apt -y autoremove
+pip3 install -U pip && pip3 install -U awscli  # For fixing awscli issue
 
 # Install zsh
 sudo apt-get install -y zsh
